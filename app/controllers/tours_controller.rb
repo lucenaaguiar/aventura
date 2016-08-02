ef2724a58caf11f43c0d620989b15810def9c43f
Marcos André <m2c.andre@gmail.com>
2016-08-01 22:03:08 -0300

    Guide create tour

---
+++ app/controllers/tours_controller.rb
@@ -0,0 +1,21 @@
+class ToursController < ApplicationController
+  def new
+    @tour = Tour.new
+  end
+
+  def create
+    @tour = Tour.new(params.require(:tour).permit(:title, :location, :category,
+                                                  :guide, :contact, :duration, 
+                                                  :amount, :description))
+    if @tour.save
+      redirect_to @tour
+    else
+      flash.now[:error] = "Não foi possível cadastra o passeio"
+      render 'new'
+    end
+  end
+
+  def show
+    @tour = Tour.find(params[:id])
+  end
+end
---
+++ spec/factories/tours.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :tour do
+    title "MyString"
+    location "MyString"
+    category "MyString"
+    guide "MyString"
+    contact "MyString"
+    duration 1
+    amount 1.5
+    description "MyText"
+  end
+end
