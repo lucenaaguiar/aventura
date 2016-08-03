c5af0323c33f62864a2ccf2d44e71ed91c3250d7
Luiz Guilherme e Leandro <lucenaaguiar@gmail.com>
2016-08-02 21:40:53 -0300

    Merge manual

--- spec/factories/guides.rb
+++ spec/factories/guides.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :guide do
-    name "Jao"
-    location "Sao Paulo"
-    phone "12 32342312"
-    email "dmara@gmail.com"
-    password "12345567"
+    name 'Jao'
+    location 'Sao Paulo'
+    phone '12 32342312'
+    email 'dmara@gmail.com'
+    password '12345567'
   end
 end
